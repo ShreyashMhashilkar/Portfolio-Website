6501cfb575d4babbdb4ec8078d4a8e76d787ed75
Shreyash Mhashilkar <75197447+ShreyashMhashilkar@users.noreply.github.com>
2024-01-16 13:08:19 +0530

    Updating code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,9 @@
-import movie from '../Images/movieapp.png';
 import invitation from '../Images/invitation.png'
 import pizza from '../Images/pizza.png'
 import wheretowatch from '../Images/wheretowatch.png'
 import { AnimatedOnScroll } from "react-animated-css-onscroll";
+import hotel from '../Images/hotel.PNG';
+import movie from '../Images/movie.PNG';
 
 const Projects = () => {
   return (
@@ -16,19 +17,43 @@ const Projects = () => {
      
 
         <div class="row row-cols-1 row-cols-md-3 g-4">
-          <div class="col" >
+        <div class="col" >
             <div class="card h-100 border_shadow"  style={{backgroundColor:'black'}}>
-              <img src={movie} style={{ height: '70%', width: '90%' }} class="card-img-top m-3" alt="..." />
+              <img src={hotel} style={{ height: '100%', width: '90%' }} class="card-img-top m-3" alt="..." />
               <div class="card-body" >
-                <h5 class="card-title" style={{color:'#ffffff'}}>MOVIE BOOKING APP</h5>
+                <h5 class="card-title" style={{color:'#ffffff'}}>HOTEL BOOKING APP</h5>
                 <p>
                 <span class="badge rounded-pill text-bg-primary mx-1 ">ReactJS</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">Node.js</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">Express.js</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">MongoDB</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">Redux</span>
                 <span class="badge rounded-pill text-bg-primary mx-1 ">Material UI</span>
                 <span class="badge rounded-pill text-bg-primary mx-1 ">HTML</span>
                 <span class="badge rounded-pill text-bg-primary mx-1 ">CSS</span>
                 </p>
-                <a href = "https://github.com/ShreyashMhashilkar/Movie-Ticket-Booking-App"><button type="button" class="btn btn-outline-primary btn-sm" ><i class="bi bi-github mx-2"></i>CODE</button></a>
-                <a href = "https://shreyash-movie-ticket-booking-app.netlify.app/"><button type="button" class="btn btn-primary btn-sm mx-2"><span class="dot mx-2"></span>LIVE</button></a>
+                <a href = "https://github.com/ShreyashMhashilkar/Hotel-Booking-Mern-App"><button type="button" class="btn btn-outline-primary btn-sm" ><i class="bi bi-github mx-2"></i>CODE</button></a>
+                <a href = "https://hotel-booking-mern-app-frontend.onrender.com/"><button type="button" class="btn btn-primary btn-sm mx-2"><span class="dot mx-2"></span>LIVE</button></a>
+              </div>
+            </div>
+          </div>
+          <div class="col" >
+            <div class="card h-100 border_shadow"  style={{backgroundColor:'black'}}>
+              <img src={movie} style={{ height: '100%', width: '90%' }} class="card-img-top m-3" alt="..." />
+              <div class="card-body" >
+                <h5 class="card-title" style={{color:'#ffffff'}}>MOVIE TICKET BOOKING APP</h5>
+                <p>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">ReactJS</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">Node.js</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">Express.js</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">MongoDB</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">Redux</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">Material UI</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">HTML</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">CSS</span>
+                </p>
+                <a href = "https://github.com/ShreyashMhashilkar/Movie-Ticket-Booking-MERN-App"><button type="button" class="btn btn-outline-primary btn-sm" ><i class="bi bi-github mx-2"></i>CODE</button></a>
+                <a href = "https://movie-ticket-booking-mern-app-frontend.onrender.com/"><button type="button" class="btn btn-primary btn-sm mx-2"><span class="dot mx-2"></span>LIVE</button></a>
               </div>
             </div>
           </div>
@@ -39,11 +64,12 @@ const Projects = () => {
                 <h5 class="card-title" style={{color:'#ffffff'}}>INVITATION CARD MAKER</h5>
                 <p>
                 <span class="badge rounded-pill text-bg-primary mx-1 ">ReactJS</span>
-                <span class="badge rounded-pill text-bg-primary mx-1 ">Bootstrap</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">Redux</span>
+                <span class="badge rounded-pill text-bg-primary mx-1 ">Material UI</span>
                 <span class="badge rounded-pill text-bg-primary mx-1 ">HTML</span>
                 <span class="badge rounded-pill text-bg-primary mx-1 ">CSS</span>
                 </p>
-                <a href = "https://github.com/ShreyashMhashilkar/Invitation-Card-Maker"><button type="button" class="btn btn-outline-primary btn-sm" ><i class="bi bi-github mx-2"></i>CODE</button></a>
+                <a href = "https://github.com/ShreyashMhashilkar/Invitation-Cards-Maker"><button type="button" class="btn btn-outline-primary btn-sm" ><i class="bi bi-github mx-2"></i>CODE</button></a>
                 <a href = "https://shreyash-invitation-card-maker.netlify.app/"><button type="button" class="btn btn-primary btn-sm mx-2"><span class="dot mx-2"></span>LIVE</button></a>
               </div>
             </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -38,7 +38,7 @@ const Skills = () => {
                                 <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
                                     <div class="progress-bar " style={{ width: '90%' }}>90%</div>
                                 </div>
-                                <h4 className="h4-white">MATERIAL UI</h4>
+                                <h4 className="h4-white">MATERAIL UI</h4>
                                 <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
                                     <div class="progress-bar " style={{ width: '85%' }}>85%</div>
                                 </div>
@@ -67,6 +67,10 @@ const Skills = () => {
                                     <div class="progress-bar bg-warning" style={{ width: '80%' }}>80%</div>
                                 </div>
                                 <h4 className="h4-white">SELENIUM</h4>
+                                <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                                    <div class="progress-bar bg-warning" style={{ width: '80%' }}>75%</div>
+                                </div>
+                                <h4 className="h4-white">NODE.JS</h4>
                                 <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
                                     <div class="progress-bar bg-warning" style={{ width: '80%' }}>75%</div>
                                 </div>
